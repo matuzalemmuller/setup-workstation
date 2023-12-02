a0ef071d0f52f0dec6ea86dae4361a15cf4b1ae9
Matuzalem Muller <30294881+matuzalemmuller@users.noreply.github.com>
2023-12-02 13:53:48 -0500

    Add more installs - webbapp-manager - joplin - terminator - teamviewer

--- wrapper-script.sh
+++ wrapper-script.sh
@@ -9,5 +9,11 @@ sudo apt install -y python3-pip pipx
 echo -e "${GREEN}Installing ansible...${NC}"
 pipx install --include-deps ansible
 
-echo -e "${GREEN}Adding ansible to PATH${NC}"
+echo -e "${GREEN}Adding ansible to PATH...${NC}"
 pipx ensurepath
+
+echo -e "${GREEN}Running the playbook...${NC}"
+ansible-playbook playbook/setup-workstation.yml
+
+echo -e "${GREEN}Setup complete!... Reboot the computer and enjoy :)${NC}"
+
