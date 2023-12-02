db7780782912cc2528dec6e0195564679df0ea51
Matuzalem Muller <30294881+matuzalemmuller@users.noreply.github.com>
2023-12-02 11:58:03 -0500

    Start wrapper script

--- setup-prerequisites.sh
+++ setup-prerequisites.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+echo -e "${GREEN}Installing prerequisites to run ansible...${NC}"
+sudo apt install -y python3-pip pipx
+
+echo -e "${GREEN}Installing ansible...${NC}"
+pipx install --include-deps ansible
+
+echo -e "${GREEN}Adding ansible to PATH${NC}"
+pipx ensurepath
