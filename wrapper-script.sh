6237ee92993e32abcefc78e62ed2834accb891bf
Matuzalem Muller <30294881+matuzalemmuller@users.noreply.github.com>
2024-01-13 14:41:03 -0500

    Add checks for first time run

--- wrapper-script.sh
+++ wrapper-script.sh
@@ -27,22 +27,26 @@ fi
 echo -e "\n${GREEN}Removing cdrom entry from /etc/apt/sources.list${NC}"
 echo $passwd | sudo -S sed -i.bak '/cdrom/d' /etc/apt/sources.list
 
-echo -e "${GREEN}Updating package repository...${NC}"
-echo $password | sudo -S apt update
+# Only install ansible if ansible-playbook command does not exist
+if ! command -v ansible-playbook &> /dev/null
+then
+    echo -e "${GREEN}Updating package repository...${NC}"
+    echo $password | sudo -S apt update
 
-# Install ansible
-echo "${GREEN}Installing prerequisites to run ansible...${NC}"
-echo $password | sudo -S apt install -y python3-pip pipx
+    # Install ansible
+    echo "${GREEN}Installing prerequisites to run ansible...${NC}"
+    echo $password | sudo -S apt install -y python3-pip pipx
 
-echo -e "${GREEN}Installing ansible...${NC}"
-pipx install --include-deps ansible
+    echo -e "${GREEN}Installing ansible...${NC}"
+    pipx install --include-deps ansible
 
-# Add ansible PATH to .bashrc
-echo -e "${GREEN}Adding ansible to PATH...${NC}"
-pipx ensurepath
+    # Add ansible PATH to .bashrc
+    echo -e "${GREEN}Adding ansible to PATH...${NC}"
+    pipx ensurepath
 
-# Add ansible to PATH for this script run
-export PATH=$PATH:/home/$username/.local/bin
+    # Add ansible to PATH for this script run
+    export PATH=$PATH:/home/$username/.local/bin
+fi
 
 # Run ansible playbook
 echo -e "${GREEN}Running the playbook...${NC}"
@@ -53,12 +57,12 @@ if [ $? -ne 0 ]; then
 fi
 
 # Complete/reboot
-echo -e "${GREEN}Setup complete! A reboot is required to complete the installation. Reboot now? (y/n)${NC}"
+echo -e "${GREEN}Setup complete! A reboot is required to the first time setup. Reboot now? (y/N)${NC}"
 read rbt
 
 if [ "$rbt" == "y" ]; then
     echo $password | sudo -S reboot now
 fi
 
-echo -e "${GREEN}You can reboot later (sudo reboot now), but do NOT start XFCE!${NC}"
+echo -e "${YELLOW}If performing your first time setup: you can reboot later (sudo reboot now), but do NOT start XFCE!${NC}"
 
