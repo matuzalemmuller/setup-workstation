fe152cb6b1d00bc64976899b385f57f452232b8f
Matuzalem Muller <30294881+matuzalemmuller@users.noreply.github.com>
2024-10-23 01:23:28 -0400

    Remove hosts file
    - Add apt install unrar-free and zip
    - Add kubecolor and micromamba install
    - Add checks to skip some software installs if already installed
    - Automate first run detection
    - Change discord install to flatpak
    - Change k9s install from web .deb to direct executable download
    - Change task names for better debugging
    - Remove hosts file; addresses are all in DNS and search domains are added to resolvconf

--- wrapper-script.sh
+++ wrapper-script.sh
@@ -8,70 +8,59 @@ NC='\033[0m'
 echo -e "${YELLOW}!!This script should be run in your local debian installation - it is NOT meant to set up a remote computer!!${NC}"
 
 # Prompt user for username and password
-echo -n "Username: "
-read username
-echo -n "Password (your user must have root privileges): "
-read -s password
-echo -ne "\nPerform first time setup? (yes/[no]): "
-read first_time_setup
+read -p "Username: " username
+read -s -p "Password (your user must have root privileges): " password
+echo
 
-if [ "${first_time_setup}" != "yes" ]; then
-    first_time_setup="no"
-fi
+# Function to execute commands with sudo
 
-# Checking if password is valid, need to be improved (but it won't be)
+sudo_exec() {
+    echo "$password" | sudo -S "$@"
+}
+
+# Checking if password is valid
 echo -e "\n${GREEN}Testing root password...${NC}"
-echo ${password} | sudo -S ls /root
-RESULT=$?
-
-if [ ${RESULT} -ne 0 ]; then
+if ! sudo_exec ls /root &>/dev/null; then
     echo -e "${YELLOW}Invalid root password!${NC}"
     exit 1
 fi
 
-# cdrom entry breaks apt update
-echo -e "\n${GREEN}Removing cdrom entry from /etc/apt/sources.list${NC}"
-echo $passwd | sudo -S sed -i.bak '/cdrom/d' /etc/apt/sources.list
+# Check if this is the first time setting up the computer
+if [[ ! -f /etc/first_time_setup ]]; then
+    # cdrom entry breaks apt update
+    echo -e "\n${GREEN}Removing cdrom entry from /etc/apt/sources.list${NC}"
+    sudo_exec sed -i.bak '/cdrom/d' /etc/apt/sources.list
+fi
 
-# Only install ansible if ansible-playbook command does not exist
-if ! command -v ansible-playbook &> /dev/null
-then
+# Install ansible if not already installed
+if ! command -v ansible-playbook &>/dev/null; then
     echo -e "${GREEN}Updating package repository...${NC}"
-    echo ${password} | sudo -S apt update
+    sudo_exec apt update
 
     # Install ansible
     echo -e "${GREEN}Installing prerequisites to run ansible...${NC}"
-    echo ${password} | sudo -S apt install -y python3-pip pipx
+    sudo_exec apt install -y python3-pip pipx
 
     echo -e "${GREEN}Installing ansible...${NC}"
     pipx install --include-deps ansible
-
-    # Add ansible PATH to .bashrc
-    echo -e "${GREEN}Adding ansible to PATH...${NC}"
-    pipx ensurepath
-
-    # Add ansible to PATH for this script run
+    pipx ensurepath --force
     export PATH=$PATH:/home/${username}/.local/bin
 fi
 
 # Run ansible playbook
 echo -e "${GREEN}Running the playbook...${NC}"
-ansible-playbook playbook/setup-workstation.yml -e "ansible_sudo_pass=${password}" -e "username=${username}" -e "first_time_setup=${first_time_setup}"
-
-if [ $? -ne 0 ]; then
+if ! ansible-playbook playbook/setup-workstation.yml -e "ansible_sudo_pass=${password}" -e "username=${username}"; then
     exit 1
 fi
 
-if [ "${first_time_setup}" == "yes" ]; then
-    # Complete/reboot
-    echo -ne "${GREEN}Setup complete! A reboot is required to the first time setup. Reboot now? ([yes]/no)${NC}: "
-    read rbt
+# Complete/reboot
+echo -ne "${GREEN}Setup complete! A reboot is required to the first time setup. Reboot now? ([yes]/no)${NC}: "
+read rbt
 
-    if [ "${rbt}" != "no" ]; then
-        if [ "${rbt}" == "yes" ] || [ -z "${rbt}" ]; then
-            echo ${password} | sudo -S reboot now
-        fi
+if [ "${rbt}" != "no" ]; then
+    if [ "${rbt}" == "yes" ] || [ -z "${rbt}" ]; then
+        echo ${password} | sudo -S reboot now
     fi
-
-    echo -e "${YELLOW}If performing your first time setup: you can reboot later (sudo reboot now), but do NOT start XFCE!${NC}"
 fi
+
+echo -e "${YELLOW}If performing your first time setup: you can reboot later (sudo reboot now), but do NOT start XFCE!${NC}"
